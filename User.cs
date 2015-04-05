a74a3d323e0084227b4e5290cea292fc760f0452
Catalin Tismanaru <catalin_271185@yahoo.com>
2015-04-05 16:17:38 +0300

    User.cs

---
+++ User.cs
@@ -0,0 +1,10 @@
+
+namespace Bulk_Email_Sender
+{
+    class User
+    {
+        public string From { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
